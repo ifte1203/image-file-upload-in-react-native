769da9ff76b715fa67eea0315ad0c3c9a870c457
iftekhar <iftekharahmed447@gmail.com>
2023-02-04 16:12:46 +0530

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Button, Alert, Image } from 'react-native';
+// File Picker
+import * as FileSystem from 'expo-file-system';
+import * as IntentLauncher from 'expo-intent-launcher';
+import * as DocumentPicker from 'expo-document-picker';
+// Image Picker
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 export default function App() {
+  const [image, setImage] = useState(null);
+
+  const openFile = async () => {
+    let res = await DocumentPicker.getDocumentAsync();
+    console.log(res);
+
+    FileSystem.getContentUriAsync(res.uri).then(cUri => {
+      console.log(cUri);
+      IntentLauncher.startActivityAsync('android.intent.action.VIEW', {
+        data: cUri,
+        flags: 1,
+      });
+    });
+     
+  }
+
+  const openImage = async () => {
+    // let res = await DocumentPicker.getDocumentAsync();
+    // console.log(res);
+
+    // FileSystem.getContentUriAsync(res.uri).then(cUri => {
+    //   console.log(cUri);
+    //   IntentLauncher.startActivityAsync('android.intent.action.VIEW', {
+    //     data: cUri,
+    //     flags: 1,
+    //   });
+    // });
+
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+     
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      <Text>Open up App.js to start working on your app!</Text>
+        <Button onPress={openFile} title='Open File'/>
+        <Button onPress={openImage} title='Open Image'/>
     </View>
   );
 }
