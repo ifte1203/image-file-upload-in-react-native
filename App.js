f4267a20843ea2d0c12d8233f6ae110f8a20fce7
iftekhar <iftekharahmed447@gmail.com>
2023-02-06 11:50:46 +0530

    New modifications

--- App.js
+++ App.js
@@ -1,72 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View, Button, Alert, Image } from 'react-native';
-// File Picker
-import * as FileSystem from 'expo-file-system';
-import * as IntentLauncher from 'expo-intent-launcher';
-import * as DocumentPicker from 'expo-document-picker';
-// Image Picker
+import React, { useState } from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import { useState } from 'react';
+import * as Permissions from 'expo-permissions';
+import axios from 'axios';
 
-export default function App() {
+const App = () => {
   const [image, setImage] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  const openFile = async () => {
-    let res = await DocumentPicker.getDocumentAsync();
-    console.log(res);
-
-    FileSystem.getContentUriAsync(res.uri).then(cUri => {
-      console.log(cUri);
-      IntentLauncher.startActivityAsync('android.intent.action.VIEW', {
-        data: cUri,
-        flags: 1,
-      });
-    });
-     
-  }
-
-  const openImage = async () => {
-    // let res = await DocumentPicker.getDocumentAsync();
-    // console.log(res);
-
-    // FileSystem.getContentUriAsync(res.uri).then(cUri => {
-    //   console.log(cUri);
-    //   IntentLauncher.startActivityAsync('android.intent.action.VIEW', {
-    //     data: cUri,
-    //     flags: 1,
-    //   });
-    // });
-
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      quality: 1,
-    });
-
-    console.log(result);
-
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
+  const handleChooseImage = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    if (status !== 'granted') {
+      alert('Sorry, we need camera roll permissions to make this work!');
+      return;
     }
-     
-  }
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+    });
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  const handleUpload = async () => {
+    setLoading(true);
+    const data = new FormData();
+    data.append('file', {
+      uri: image,
+      name: 'file',
+      type: 'image/jpeg'
+    });
+    try {
+      const response = await axios.post('http://192.168.1.19/MyAPI/setProfile.php', data, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      console.log(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
-      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-      <Text>Open up App.js to start working on your app!</Text>
-        <Button onPress={openFile} title='Open File'/>
-        <Button onPress={openImage} title='Open Image'/>
+      {image ? (
+        <Image source={{ uri: image }} style={styles.previewImage} />
+      ) : (
+        <Text style={styles.promptText}>No Image Selected</Text>
+      )}
+      <TouchableOpacity style={styles.button} onPress={handleChooseImage}>
+        <Text style={styles.buttonText}>Choose Image</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={handleUpload}>
+        <Text style={styles.buttonText}>{loading ? 'Uploading...' : 'Upload'}</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50
   },
+  previewImage: {
+    width: 200,
+    height: 200,
+    borderRadius: 10,
+    marginBottom: 10
+  },
+  promptText: {
+    fontSize: 20,
+    marginBottom: 10
+  },
+  button: {
+    padding: 10,
+    backgroundColor: '#1E90FF',
+    borderRadius: 5,
+    marginTop: 10
+  },
+  buttonText: {
+    color: '#FFF'
+  }
 });
+
+export default App;
